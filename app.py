c1f801b3b4b0a7d9f4d16a5688d362bbe14f1e3f
LuizSuzim <luiz_c9@hotmail.com>
2026-02-13 13:22:44 -0300

    change

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 from database import criar_tabelas, inserir_cliente, listar_clientes
 
 app = Flask(__name__)
 
 criar_tabelas()
+
+
+@app.route("/")
+def index():
+    return "API Sistema de Atendimentos rodando!"
+
+
+@app.route("/api/clientes", methods=["GET"])
+def get_clientes():
+    clientes = listar_clientes()
+
+    resultado = []
+    for cliente in clientes:
+        resultado.append({
+            "id": cliente[0],
+            "nome": cliente[1],
+            "telefone": cliente[2]
+        })
+
+    return jsonify(resultado)
+
 
 @app.route("/api/clientes", methods=["POST"])
 def post_cliente():
@@ -17,6 +38,7 @@ def post_cliente():
 
     inserir_cliente(nome, telefone)
     return jsonify({"mensagem": "Cliente cadastrado com sucesso"}), 201
-if __name__ == '__main__':
-    app.run(debug=True)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
