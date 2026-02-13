6c6efa53b6337875044e8d4ff272b28f95a6ef9e
LuizSuzim <luiz_c9@hotmail.com>
2026-02-13 13:10:42 -0300

    changes

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
-from database import criar_tabelas
+from database import criar_tabelas, inserir_cliente, listar_clientes
 
 app = Flask(__name__)
 
 criar_tabelas()
 
-@app.route('/')
+@app.route("/api/clientes", methods=["POST"])
+def post_cliente():
+    dados = request.get_json()
 
-def index():
-    return render_template('index.html')
+    nome = dados.get("nome")
+    telefone = dados.get("telefone")
+
+    if not nome or not telefone:
+        return jsonify({"erro": "Nome e telefone são obrigatórios"}), 400
+
+    inserir_cliente(nome, telefone)
+    return jsonify({"mensagem": "Cliente cadastrado com sucesso"}), 201
 if __name__ == '__main__':
     app.run(debug=True)
 
--- database.py
+++ database.py
@@ -21,3 +21,26 @@ def criar_tabelas():
                    FOREIGN KEY (cliente_id) REFERENCES clientes (id))""")
     conexao.commit()
     conexao.close()
+
+def inserir_cliente(nome, telefone):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+        "INSERT INTO clientes (nome, telefone) VALUES (?, ?)",
+        (nome, telefone)
+    )
+
+    conexao.commit()
+    conexao.close()
+
+
+def listar_clientes():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT id, nome, telefone FROM clientes")
+    clientes = cursor.fetchall()
+
+    conexao.close()
+    return clientes
